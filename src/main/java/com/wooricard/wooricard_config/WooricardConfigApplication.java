fbe8c3ca9f3758076f4d9bf2683e7d80022ac444
Minyeong <skaalsdud7@gmail.com>
2026-03-20 11:29:32 +0900

    feat: Spring Cloud Config 서버 구축 및 Git 레포지토리 연동

--- src/main/java/com/wooricard/wooricard_config/WooricardConfigApplication.java
+++ src/main/java/com/wooricard/wooricard_config/WooricardConfigApplication.java
@@ -2,12 +2,14 @@ package com.wooricard.wooricard_config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer    // Config 서버 역할 활성화
+@EnableDiscoveryClient // 유레카 클라이언트로 등록
 public class WooricardConfigApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WooricardConfigApplication.class, args);
 	}
-
 }
